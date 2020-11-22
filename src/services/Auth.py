d17bb0f1635bf9bd208728b7d5fd4b5b6885c610
Christoffer Lundström <lulleh_@hotmail.com>
2020-11-22 21:14:34 +0100

    changed script path

--- src/services/Auth.py
+++ src/services/Auth.py
@@ -6,30 +6,32 @@ from subprocess import Popen, PIPE
 def validate(request):
     try:
 
-        expectedHash = request.headers.get('X-Hub-Signature').replace("sha1=", "")
+        expected_hash = request.headers.get('X-Hub-Signature').replace("sha1=", "")
 
-        calculatedHash = hmac.new(os.environ.get('WEBHOOK_KEY','').encode("utf-8"), msg=request.data, digestmod=hashlib.sha1).hexdigest()
+        calculated_hash = hmac.new(os.environ.get('WEBHOOK_KEY', '').encode("utf-8"), msg=request.data,
+                                   digestmod=hashlib.sha1).hexdigest()
 
-        isAllowed = hmac.compare_digest(calculatedHash, expectedHash)
-        isMaster = (request.json['ref'] == 'refs/heads/master')
+        is_allowed = hmac.compare_digest(calculated_hash, expected_hash)
+        is_master = (request.json['ref'] == 'refs/heads/master')
 
-        if isAllowed and isMaster:
+        if is_allowed and is_master:
             exitcode, out, err = get_exitcode_stdout_stderr(os.environ.get('REPO'))
             if exitcode == 1:
                 return 'Server error: ' + str(err), 500
             else:
-                return 'OK', 200
-        return f'Bad request: Allowed {isAllowed}, Master: {isMaster}', 400
+                res = {"OK": out, "exitcode": exitcode, "error": err}
+                return res, 200
+        return f'Bad request: Allowed {is_allowed}, Master: {is_master}', 400
 
     except Exception as e:
         return 'Forbidden', 403
 
 
-def get_exitcode_stdout_stderr(cmd):
+def get_exitcode_stdout_stderr(script_path):
     """
     Execute the external command and get its exitcode, stdout and stderr.
     """
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    proc = Popen(script_path, stdout=PIPE, stderr=PIPE, shell=True)
     out, err = proc.communicate()
     exitcode = proc.returncode
 
